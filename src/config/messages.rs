e191b9e5f298997e19383d5a300a5d00fad19baf
Tyson Roberts <nall@google.com>
2026-05-08 12:46:09 +0100

    fix: silence dead code warnings in messages config

--- src/config/messages.rs
+++ src/config/messages.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Config {
     pub home: SectionPool,
@@ -10,6 +11,7 @@ pub struct Config {
 }
 
 impl Config {
+    #[allow(dead_code)]
     pub fn load(path: std::path::PathBuf) -> anyhow::Result<Self> {
         let content = std::fs::read_to_string(path)?;
         let config: Config = toml::from_str(&content)?;
@@ -17,17 +19,20 @@ impl Config {
     }
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SectionPool {
     pub pool: Vec<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct CdConfig {
     pub pool: Vec<String>,
     pub context: HashMap<String, String>,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct DiscoveryConfig {
     pub template: String,
